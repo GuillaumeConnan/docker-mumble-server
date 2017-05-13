19e279774201980c54dd7104ad02f888d38e6d67
SilentHunter44 <guillaume.connan44@gmail.com>
2017-05-13 22:54:52 +0200

    Add default behaviour when no vars passed

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM debian:stretch
 
 MAINTAINER Guillaume CONNAN "guillaume.connan44@gmail.com"
 
-LABEL version="0.3.1"              \
+LABEL version="0.3.2"              \
       mumble_version="1.2.18-1"
 
 ENV DEBIAN_FRONTEND noninteractive
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-${MS_CONFIGFILE:=/mumble-server/mumble-server.ini}
-${MS_PIDFILE:=/var/run/mumble-server/mumble-server.pid}
-
 mkdir -p ${MS_PIDPATH:=/var/run/mumble-server/}
-
 ${MS_PIDFILE:=$MS_PIDPATH/mumble-server.pid}
 
+${MS_CONFIGFILE:=/mumble-server/mumble-server.ini}
+${MS_DATABASE:=/mumble-server/mumble-server.sqlite}
+
 # Generate config if needed
-if [ ! -f "$MS_CONFIGFILE" ]; then
-    echo "database=${MS_DATABASE:=/mumble-server/mumble-server.sqlite}"                                       >  $MS_CONFIGFILE
+if [ ! -f "$MS_CONFIGFILE" ]
+then
+    echo "database=$MS_DATABASE"                                                                              >  $MS_CONFIGFILE
     echo "icesecretwrite="                                                                                    >> $MS_CONFIGFILE
     echo "logfile="                                                                                           >> $MS_CONFIGFILE
     echo "pidfile=$MS_PIDFILE"                                                                                >> $MS_CONFIGFILE
@@ -22,16 +22,31 @@ if [ ! -f "$MS_CONFIGFILE" ]; then
     echo "[Ice]"                                                                                              >> $MS_CONFIGFILE
     echo "Ice.Warn.UnknownProperties=1"                                                                       >> $MS_CONFIGFILE
     echo "Ice.MessageSizeMax=65536"                                                                           >> $MS_CONFIGFILE
+else
+    # If config file exists, only set serverpassword if defined
+    if [ -n "$MS_SERVERPASSWORD" ]
+    then
+        sed -i "s/serverpassword=.*/serverpassword=$MS_SERVERPASSWORD/" $MS_CONFIGFILE
+    fi
 fi
 
 # Remove old PID file
-if [ -f "$MS_PIDFILE" ]; then
+if [ -f "$MS_PIDFILE" ]
+then
     rm -f $MS_PIDFILE
 fi
 
 # Setting supw
-if [ -n "$MS_SUPW" ]; then
+if [ -n "$MS_SUPW" ]
+then
     /usr/sbin/murmurd -fg -ini $MS_CONFIGFILE -supw "$MS_SUPW"
+else
+	if [ ! -f "$MS_DATABASE" ]
+	then
+		MS_SUPW=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+		/usr/sbin/murmurd -fg -ini $MS_CONFIGFILE -supw "$MS_SUPW"
+		echo $MS_SUPW > /mumble-server/supw
+	fi
 fi
 
 chown -R mumble-server:mumble-server /mumble-server $MS_PIDPATH
